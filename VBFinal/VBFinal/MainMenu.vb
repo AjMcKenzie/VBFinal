b863c1ce12df82cc95760694f7d71d639039928b
Andrew <heldiggris7@gmail.com>
2017-12-19 10:32:06 -0600

    update LevelSelect

--- VBFinal/VBFinal/MainMenu.vb
+++ VBFinal/VBFinal/MainMenu.vb
@@ -1,3 +1,31 @@
 ﻿Public Class MainMenu
 
+    Dim second As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Interval = 1000
+        Timer1.Start() 'Timer starts functioning
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        picLogo.Visible = False
+        lblLogoName.Visible = False
+
+        second = second + 1
+        If second >= 2 Then
+            btnStart.Visible = True
+            btnExit.Visible = True
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim Box = New LevelSelect
+        Box.Show()
+        Me.Visible = False
+    End Sub
 End Class
