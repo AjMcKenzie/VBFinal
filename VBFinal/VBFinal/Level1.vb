a62372ed2a26706fc83b12075ab2a8803cf6f33f
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-12-20 09:33:12 -0600

    update level 1-2

--- VBFinal/VBFinal/Level1.vb
+++ VBFinal/VBFinal/Level1.vb
@@ -3,5 +3,6 @@
     Private Sub PictureBox14_Click(sender As Object, e As EventArgs) Handles PictureBox14.Click
         Dim Box = New LevelSelect
         Box.Show()
+        Me.Close()
     End Sub
 End Class
